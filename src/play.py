8add7b2a04f5f481085f0dde9655e136147d6f17
Jacob Howes <44398267+jwhowes@users.noreply.github.com>
2025-01-29 21:51:18 -0500

    Implemented toggleable thinking time (#2)

---
+++ src/play.py
@@ -0,0 +1,96 @@
+import time
+import torch
+
+from multiprocessing import Process, Event, Value
+
+from .gym import State
+from .model import ConvModel, ConvModelConfig
+from .mcts import MCTS
+
+
+class Player:
+    def __init__(
+            self, model_config: ConvModelConfig, model_path: str, thinking_time: float, sims_per_move: int,
+            computer_first: bool = False
+    ):
+        self.user_input = Event()
+
+        self.computer_output_request = Event()
+        self.computer_output_sent = Event()
+
+        self.action = Value('i', 0)
+
+        self.model_config = model_config
+        self.model_path = model_path
+
+        self.thinking_time = thinking_time
+        self.sims_per_move = sims_per_move
+        self.computer_first = computer_first
+
+    def mcts_worker(self):
+        mcts = MCTS(sims_per_move=self.sims_per_move)
+
+        model = ConvModel.from_config(self.model_config)
+        model.load_state_dict(torch.load(self.model_path, weights_only=True))
+        model.eval()
+        model.requires_grad_(False)
+
+        player = not self.computer_first
+        while mcts.root.winner is None:
+            action = None
+
+            if player and self.user_input.is_set():
+                player = False
+                action = self.action.value
+                self.user_input.clear()
+
+            if not player and self.computer_output_request.is_set():
+                player = True
+                self.computer_output_request.clear()
+                action = mcts.root.num_visits.argmax()
+
+                self.action.value = int(action)
+                self.computer_output_sent.set()
+
+            if action is not None:
+                mcts.step(action)
+
+            mcts.search(model)
+
+    def play(self):
+        mcts_worker = Process(target=self.mcts_worker)
+        mcts_worker.start()
+
+        state = State.initial()
+
+        player = not self.computer_first
+        while state.winner() is None:
+            state.display()
+            if player:
+                player = False
+                action = int(input("Enter your move: "))
+                self.action.value = action
+                self.user_input.set()
+            else:
+                player = True
+
+                time.sleep(self.thinking_time)
+                self.computer_output_request.set()
+
+                self.computer_output_sent.wait()
+                self.computer_output_sent.clear()
+                action = self.action.value
+
+                print(f"Computer move: {int(action)}")
+
+            state = state.step(action)
+
+        state.display()
+        winner = state.winner()
+
+        if winner == 0:
+            print("It's a tie!")
+        elif winner == 1:
+            print("Computer wins!" if self.computer_first else "Player wins!")
+        else:
+            print("Player wins!" if self.computer_first else "Computer wins!")
